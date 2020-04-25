03babd06dcf46774b00faa87ad6f17204ee0d64e
zesheee <ipadamov82@gmail.com>
2020-04-25 13:08:22 +0900

    "init"

--- bot.py
+++ bot.py
@@ -1,87 +1,69 @@
 import telebot
 from telebot import types
-#from mongodb import mdb
-
 import config
 
 bot = telebot.TeleBot(config.token)
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
-keyboard_phone = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-button_phone = types.KeyboardButton(text="Отправить телефон", request_contact=True)
-keyboard_phone.add(button_phone)
-
 
 name = ''
 surname = ''
+patronym = ''
+adres = ''
+t = 0
+job=''
 age = 0
 
-
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, {}, ты написал мне /start', reply_markup=keyboard1)
-    phone(message)
-
-
-def phone(message):
-    bot.send_message(message.chat.id, 'Номер телефона', reply_markup=keyboard_phone)
-
-@bot.message_handler(content_types=['contact'])
-def contact(message):
-    if message.contact is not None:
-        print(message.contact.phone_number)
-
-@bot.message_handler(commands=['reg'])
 def start(message):
-    bot.send_message(message.from_user.id, "Как тебя зовут?")
+    bot.send_message(message.from_user.id, "Как ваше имя?")
     bot.register_next_step_handler(message, get_name)
 
 
 def get_name(message):
     global name
     name = message.text
-    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.send_message(message.from_user.id, 'Какая у вас фамилия?')
     bot.register_next_step_handler(message, get_surname)
 
 
 def get_surname(message):
     global surname
     surname = message.text
-    bot.send_message(message.from_user.id, 'Сколько тебе лет?')
+    bot.send_message(message.from_user.id, 'Отчество?')
+    bot.register_next_step_handler(message, get_patronym)
+
+
+def get_patronym(message):
+    global patronym
+    patronym = message.text
+    bot.send_message(message.from_user.id, 'Пожалуйста, укажите адрес проживания по прописке')
+    bot.register_next_step_handler(message, get_adres)
+
+
+def get_adres(message):
+    global adres
+    adres = message.text
+    bot.send_message(message.from_user.id, 'Пожалуйста укажите температуру через точку \n Пример: 36.6')
+    bot.register_next_step_handler(message, get_temperature)
+
+
+def get_temperature(message):
+    global t
+    t = float(message.text)
+    bot.send_message(message.from_user.id, 'Пожалуйста, укажите место вашей текущей работы')
+    bot.register_next_step_handler(message, get_job)
+
+
+def get_job(message):
+    global job
+    job = message.text
+    bot.send_message(message.from_user.id, 'И ваш возраст')
     bot.register_next_step_handler(message, get_age)
 
 
 def get_age(message):
     global age
-    if age == 0:
-        try:
-            age = int(message.text)
-            question(message)
-        except Exception:
-            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
-            bot.register_next_step_handler(message, get_age)
-    else:
-        bot.register_next_step_handler(message, question)
-
-
-def question(message):
-    keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')  # кнопка «Да»
-    keyboard.add(key_yes)  # добавляем кнопку в клавиатуру
-    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
-    keyboard.add(key_no)
-    question = 'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?'
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "yes":  # call.data это callback_data, которую мы указали при объявлении кнопки
-        # код сохранения данных, или их обработки
-        bot.send_message(call.message.chat.id, 'Запомню : )');
-    elif call.data == "no":
-        print("123")  # asd
-
+    age = int(message.text)
+    bot.send_message(message.from_user.id, 'Спасибо за сотрудничество!')
 
 bot.polling()
